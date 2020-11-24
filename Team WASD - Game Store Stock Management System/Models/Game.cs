c281d10833b9355b97026be9d286574701a51a1b
weibinw219 <weibinw0219@gmail.com>
2020-11-24 01:15:32 -0500

    Add database

---
+++ Team WASD - Game Store Stock Management System/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Team_WASD___Game_Store_Stock_Management_System.Models
+{
+    [Table("tblGame")]
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        public string GameTitle { get; set; }
+    }
+}
---
+++ Team WASD - Game Store Stock Management System/Models/GameStoreDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Team_WASD___Game_Store_Stock_Management_System.Models
+{
+    public class GameStoreDBContext : DbContext
+    {
+        public GameStoreDBContext() : base("GameStoreDBContext") { }
+
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
